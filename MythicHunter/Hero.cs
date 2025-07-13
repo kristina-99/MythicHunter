ab495a3bcc1bcc50197caec048dca45891e506e3
kristina-99 <kristina_atan@abv.bg>
2025-07-13 12:20:20 +0300

    * added a Hero class * added getters and setters for the properties of the Hero class

---
+++ MythicHunter/Hero.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MythicHunter
+{
+    internal class Hero
+    {
+        private int power;
+        private int mana;
+        private int health;
+
+        public int Power 
+        {
+            get
+            {
+                return this.power;
+            }
+            set
+            {
+                this.power = value;
+            }
+        }
+
+        public int Mana
+        {
+            get
+            {
+                return this.mana;
+            }
+            set
+            {
+                this.mana = value;
+            }
+        }
+
+        public int Health
+        {
+            get 
+            {
+                return this.health;
+            }
+            set
+            {
+                this.health = value;
+            }
+        }
+    }
+}
---
+++ MythicHunter/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MythicHunter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MythicHunter")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d3d1cf0d-71da-4a33-97bd-50b7739a99d1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
