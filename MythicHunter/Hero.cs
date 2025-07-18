c6ca006d19447bd22bc31a837178ce7ee0db29a8
kristina-99 <kristina_atan@abv.bg>
2025-07-18 15:36:19 +0300

    * added logic based on input to the Move method in the Hero class

--- MythicHunter/Hero.cs
+++ MythicHunter/Hero.cs
@@ -55,5 +55,60 @@ namespace MythicHunter
         {
             inventory.Add(newItem);
         }
+
+        public void Move(char inputKey)
+        {
+            switch (inputKey)
+            {
+                case 'w':
+                    {
+                        if (currentYposition < 3)
+                        {
+                            currentYposition++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("The hero can't leave the map. Please move to another direction!");
+                        }
+                        break;
+                    }
+                case 's':
+                    {
+                        if (currentYposition > 0)
+                        {
+                            currentYposition--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("The hero can't leave the map. Please move to another direction!");
+                        }
+                        break;
+                    }
+                case 'd':
+                    {
+                        if (currentYposition < 5)
+                        {
+                            currentXposition++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("The hero can't leave the map. Please move to another direction!");
+                        }
+                        break;
+                    }
+                case 'a':
+                    {
+                        if (currentYposition > 0)
+                        {
+                            currentXposition--;
+                        }
+                        else
+                        {
+                            Console.WriteLine("The hero can't leave the map. Please move to another direction!");
+                        }
+                        break;
+                    }
+            }
+        }
     }
 }
