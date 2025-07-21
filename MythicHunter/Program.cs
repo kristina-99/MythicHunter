da0af0549d6247fac6a43c1bb29c40e14b5948dd
kristina-99 <kristina_atan@abv.bg>
2025-07-21 23:09:30 +0300

    * created a map in the Program class main method * initialized a Hero object and put it on the map

--- MythicHunter/Program.cs
+++ MythicHunter/Program.cs
@@ -10,6 +10,33 @@ namespace MythicHunter
     {
         static void Main(string[] args)
         {
+            bool gameOver = false;
+
+            char[][] map = new char[][]
+            {
+                new char[]{ '.', '.', '.', '.', '.', '.'},
+                new char[]{ '.', '.', 'M', '.', '.', '.'},
+                new char[]{ '.', '.', '#', '.', '#', '#'},
+                new char[]{ '.', 'I', '#', '.', '.', '.'}
+            };
+
+            Hero myHero = new Hero();
+
+            map[myHero.CurrentXPosition][myHero.CurrentXPosition] = 'H';
+
+            while (!gameOver)
+            {
+                for (int i = 0; i < map.Length; i++)
+                {
+                    for (int j = 0; j < map[i].Length; j++)
+                    {
+                        Console.Write(map[i][j]);
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("Please enter the next direction (W, A, S or D):");
+            }
         }
     }
 }
